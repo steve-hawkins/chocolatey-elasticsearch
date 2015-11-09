14bc8adc384c994326a985a022ebcf7a0ea41576
Richard Pijnenburg <richard@ispavailability.com>
2015-11-09 21:59:35 +0000

    Update to ES 1.7.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
-Install-ChocolateyZipPackage 'elasticsearch' 'http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage 'elasticsearch' 'http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.2.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyPath "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\elasticsearch-1.2.1\bin"
+Install-ChocolateyPath "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\elasticsearch-1.7.2\bin"
