993220f6bff9184a99dc579b4350505c67f40514
Toby McLaughlin <toby@jarpy.net>
2016-04-05 10:04:01 +1000

    Update to v2.2.2.

--- package/tools/chocolateyInstall.ps1
+++ package/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
-Install-ChocolateyZipPackage 'elasticsearch' 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-2.2.1.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage 'elasticsearch' 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-2.2.2.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyPath "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\elasticsearch-2.2.1\bin"
+Install-ChocolateyPath "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\elasticsearch-2.2.2\bin"
